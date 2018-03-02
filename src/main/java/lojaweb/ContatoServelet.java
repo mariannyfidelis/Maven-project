508c1a7c2d761df6021592adabf64f40e85ff060
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-02 10:30:32 -0300

    Criando servlet contato

---
+++ src/main/java/lojaweb/ContatoServelet.java
@@ -0,0 +1,26 @@
+package lojaweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/contato")
+public class ContatoServelet  extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 26514546089326548L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		PrintWriter writer = resp.getWriter();
+		writer.print("<html><body><h2> Entre em contato </h2></body></html>");
+	}
+}
