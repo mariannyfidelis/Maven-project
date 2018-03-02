4f7313ba6389fc3fab502f54c7aeb80932b7d6b8
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-02 10:31:46 -0300

    fechando o writer

--- src/main/java/lojaweb/ContatoServelet.java
+++ src/main/java/lojaweb/ContatoServelet.java
@@ -2,10 +2,9 @@ package lojaweb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,5 +21,6 @@ public class ContatoServelet  extends HttpServlet{
 
 		PrintWriter writer = resp.getWriter();
 		writer.print("<html><body><h2> Entre em contato </h2></body></html>");
+		writer.close();
 	}
 }
